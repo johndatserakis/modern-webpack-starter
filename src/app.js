4783359ef6fa06e6fa8902b91abe2e4c50c75d48
John Datserakis <johndatserakis@gmail.com>
2017-08-20 02:13:39 -0400

    updates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 require('./assets/images/favicon.png')
 require('./assets/css/app.scss');
-const imdImage = require('./assets/images/imd-profile-transparent.png')
+const imdImage = require('./assets/images/mws-profile.png')
 
 import 'bootstrap';
 
@@ -14,17 +14,17 @@ if (path === '/') {
     image.setAttribute('src', imdImage)
     main.appendChild(image)
 
-    main.innerHTML += 'Howdy. You\'re on the home page. <a href="/about">Click here</a> to view the about page.'
+    main.innerHTML += 'Howdy. You\'re on the HOME page. <a href="/about">Click here</a> to view the about page.'
 }
 
 if (path === '/about') {
     let main = document.getElementById('app')
 
     let image = document.createElement('img')
-    image.setAttribute('src', 'assets/images/imd-profile-transparent.png')
+    image.setAttribute('src', imdImage)
     main.appendChild(image)
 
-    main.innerHTML += 'Howdy. You\'re on the about page. <a href="/">Click here</a> to view the home page.'
+    main.innerHTML += 'Howdy. You\'re on the ABOUT page. <a href="/">Click here</a> to view the home page.'
 }
 
 
